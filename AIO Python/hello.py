3eecc836ef581503ef789026cbf19fcbbcc42403
Idriss-cpu <elalamiidriss@gmail.com>
2021-12-21 23:13:21 +0100

    modif 21/12/21

--- AIO Python/hello.py
+++ AIO Python/hello.py
@@ -1,9 +1,9 @@
 # This is a Python comment in my first Python app.
 # This variable contains an integer
-quantity = 14
+quantity = 142
 # This variable contains a float
 unite_price = 26.99
 # This variable contains the result of multiplying quantity times unit price
-extended_price = quantity * unite_price
+extended_price = quantity * unite_price * 0.95
 # Show the results
 print(extended_price)
